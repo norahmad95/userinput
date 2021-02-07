81b4baaf1716ca75d2260ce68aa7c793f95be6fa
norahmad95 <artistnor95@gmail.com>
2021-02-07 15:32:08 +0200

    userinput

---
+++ content.js
@@ -0,0 +1,17 @@
+let yourName = prompt('please inter your name', "<name gose her>");
+
+alert(yourName);
+
+let joke = prompt('do you want to read jok?', "<yes or no >");
+
+alert(joke);
+
+let no = prompt('do you laugh?', "<yes or no >");
+
+alert(no);
+
+let yes = prompt('are you happy now ?', "<yes or no >")
+
+alert(yes)
+
+console.log('qustion ======>', yourName, joke, no, yes)
